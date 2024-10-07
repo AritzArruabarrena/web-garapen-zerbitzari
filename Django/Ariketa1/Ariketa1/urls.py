050ebd9958ceba3d4d454ce1125f979cfe2ebe29
Aritz <arruabarrena.aritz@uni.eus>
2024-10-07 19:46:45 +0200

    gitignore

--- Django/Ariketa1/Ariketa1/urls.py
+++ Django/Ariketa1/Ariketa1/urls.py
@@ -20,7 +20,8 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('AriketaDjango1.urls'))
+    path('', include('AriketaDjango1.urls')),
+    path('' , include('Kotxeak.urls'))
 
 ]
 
--- Django/Ariketa1/Kotxeak/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KotxeakConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Kotxeak'
