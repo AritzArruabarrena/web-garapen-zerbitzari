17d82ba95ec8ea9a7339851f48a9e864b85e84cf
Aritz <arruabarrena.aritz@uni.eus>
2024-10-09 21:08:42 +0200

    ASX

--- Django/Ariketa1/Kotxeak/urls.py
+++ Django/Ariketa1/Kotxeak/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  # Importar las vistas desde Kotxeak
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Ruta principal para Kotxeak
+    path('zerrenda/', views.index, name='index'),  # Ruta principal para Kotxeak
+    path('', views.alokatu_list,name="alokatu_list")
 ]
--- Django/Ariketa1/Kotxeak/views.py
+++ Django/Ariketa1/Kotxeak/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import *
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def alokatu_list(request):
+    alokatuZerrenda=Alokatu.objects.all()
+    return render(request,'alokatu/alokatu_list.html' , {'alokatu':alokatuZerrenda})
