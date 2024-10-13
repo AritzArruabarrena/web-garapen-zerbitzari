66b7e25bdbaae33ef219f772ab9ae9abc63ac922
Aritz <arruabarrena.aritz@uni.eus>
2024-10-13 13:28:18 +0200

    aldaketak

--- Django/Ariketa1/Kotxeak/urls.py
+++ Django/Ariketa1/Kotxeak/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views  # Importar las vistas desde Kotxeak
 
 urlpatterns = [
-    path('zerrenda/', views.index, name='index'),  # Ruta principal para Kotxeak
-    path('', views.alokatu_list,name="alokatu_list")
+    path('zerrenda', views.index, name='index'),  # Ruta principal para Kotxeak
+    path('', views.alokatu_list,name="alokatu_list"),
+    path("kotxeak/", views.kotxeko_datuak, name="kotxeko-datuak"),
+    path('', views.index, name='index2'),
+    path('pertsona/', views.pertsona_datuak, name='pertsona-datuak'),
+    path('kotxeak/ikusi/', views.kotxeko_datuak_ikusi, name='kotxeko-datuak-ikusi'),
+    path('kotxeak/', views.kotxeko_datuak_list,name="kotxeko_datuak_list"),
 ]
--- Django/Ariketa1/Kotxeak/views.py
+++ Django/Ariketa1/Kotxeak/views.py
@@ -11,3 +11,19 @@ def index(request):
 def alokatu_list(request):
     alokatuZerrenda=Alokatu.objects.all()
     return render(request,'alokatu/alokatu_list.html' , {'alokatu':alokatuZerrenda})
+
+def kotxeko_datuak(request):
+    return render(request, 'kotxeak/kotxeak.html')
+
+def kotxeko_datuak_ikusi(request):
+    return render(request, 'kotxeak/kotxeko-datuak.html')
+
+def kotxeko_datuak_list(request):
+    kotxeZerrenda = Kotxe.objects.all()
+    return render(request, 'kotxeak/kotxeak_list.html', {'kotxe': kotxeZerrenda})
+
+
+def pertsona_datuak(request):
+    return render(request, 'pertsonak/pertsona.html')
+
+
